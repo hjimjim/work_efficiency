3b06caf57fbe67c69be6c79e5e2e8c6cd81f4d88
Jimin Heo <jimjim.heo@gmail.com>
2020-04-08 23:52:15 +0900

    first commit

---
+++ data_parse.py
@@ -0,0 +1,67 @@
+import pprint
+data = open("data.txt", 'r')
+lines = data.readlines()
+
+hosts = []
+for line in lines :
+    check_host = []
+    check_sw = False
+    hostname = line.split("/")[0]
+    switch = line.split("/")[1]
+    nic = line.split("/")[2]
+
+    if len(hosts) == 0 :
+        host_dic = {
+            'name' : hostname,
+            'switches' : [{
+                'name' : switch,
+                'nic' : [nic],
+                'vlan' : []
+            }]
+        }
+        hosts.append(host_dic)
+    else :
+        for host in hosts :
+            if hostname == host.get('name') :
+                check_host = [host]
+                for sw in host.get('switches') :
+                    if switch == sw.get('name') :
+                        sw.get('nic').append(nic)
+                        check_sw = True
+        
+        if len(check_host) == 0 :
+            host_dic = {
+                'name' : hostname,
+                'switches' : [{
+                    'name' : switch,
+                    'nic' : [nic],
+                    'vlan' : []
+                }]
+            }
+            hosts.append(host_dic)
+        elif not check_sw :
+            new_switch = { 
+                'name' : switch,
+                'nic' : [nic],
+                'vlan' : []
+            }
+            check_host.pop().get('switches').append(new_switch)
+
+                
+
+data = open("data2.txt", 'r')
+lines = data.readlines()
+
+for line in lines :
+    hostname = line.split("/")[0]
+    switch = line.split("/")[1]
+    vlan = line.split("/")[2]
+
+    for host in hosts :
+        if hostname == host.get('name') :
+            for sw in host.get('switches') :
+                if switch == sw.get('name') :
+                    sw.get('vlan').append(vlan)
+
+
+pprint.pprint(hosts)
